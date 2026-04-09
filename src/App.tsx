a9b0246d1148d01b5b7cdc95db7fd9080cc2a0d3
tuprikov <tuprikov@gmail.com>
2026-04-09 22:42:37 +0200

    Initial setup.

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import About from "./pages/About"
+import Home from "./pages/Home"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+])
+
+const App = () => {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
